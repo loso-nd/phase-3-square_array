59ea478c7c96f2e003530a984b4d08157e797948
Nahum <losond8@gmail.com>
2021-07-09 23:53:06 -0400

    MNote does not return a new array. We must create a new array and push the results into the new arr inside the block.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,20 @@
+require 'pry'
 def square_array(array)
   # your code here
+  arr = []
+  array.each{|i| arr << i**2}
+  arr #must call the arr in order to see results
+  # binding.pry
 end
+
+
+
+
+# def square_array(array)
+#   arr = []
+#   array.each { |i| arr << i ** 2 }
+#   arr
+# end
+
+# my_arr = [1, 2]
+# p square_array(my_arr) #=> [1, 4]
